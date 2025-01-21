2dc042f5ba00c752e02dc2f4ea8dc7de937b45f0
AndreScaffidi <andre.scaffidi@adelaide.edu.au>
2025-01-21 14:02:15 +0100

    .

--- examples/Seven_modes_EE_v7/test_equalization_7_modes.py
+++ examples/Seven_modes_EE_v7/test_equalization_7_modes.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Nov 19 16:46:20 2024
 
-@author: sspringe
+@author: sspringe137, AndreScaffidi
 """
 
 import numpy as np
@@ -15,7 +15,8 @@ import sys
 module_path = '../../eagleeye'
 sys.path.append(module_path)
 import EagleEye_v7
-import From_data_to_binary
+import sys
+# import From_data_to_binary
 
 # Custom plotting settings
 sns.set(style="darkgrid")
@@ -78,100 +79,119 @@ for size, center in zip(anomaly_sizes, positions):
     overdensities.append(anomaly)
     local_i = local_i + 1
 
-
-
 # Combine the test set with the anomalies
 data_with_anomaly = np.vstack([data_with_anomaly] + overdensities)
 
 
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# EagleEye
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+res = EagleEye_v7.Soar(reference_data, data_with_anomaly, result_dict={}, K_M=100, critical_quantile=1-1e-5, post_process=True, num_cores=100, validation=None, partition_size=100,smoothing=3)
+
+res_size = sys.getsizeof(res) / 1e9
+print(f"Memory size of res: {res_size:.2f} Gigabytes")
+
 #%%
 
 
-#%% get the thresholds
-
-VALIDATION                                     = None        # Number of samples to use for validation 
-K_M                                            = 1000            # Maximum value of kstar to consider
-KSTAR_RANGE                                    = range(20, K_M) # Range of kstar values to consider
-NUMBER_CORES                                   = 12  
-PARTITION_SIZE                                 = 100
-#binary_sequences                               = From_data_to_binary.create_binary_array_cdist(test_samples, reference_samples, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
-num_sequences = 200000
-binary_sequences = np.random.binomial(n=1, p=0.5, size=(num_sequences, K_M))
-stats                                          = EagleEye_v7.calculate_p_values(binary_sequences, kstar_range=KSTAR_RANGE, validation=VALIDATION)
-
-
-Upsilon_i_plus = stats['Upsilon_i_plus']
-Upsilon_i_minus = stats['Upsilon_i_minus']
-kstar_plus = stats['kstar_plus']
-kstar_minus = stats['kstar_minus']
-
-# Unpack validation statistics
-Upsilon_i_plus_val = stats['Upsilon_i_Val_plus']
-Upsilon_i_minus_val = stats['Upsilon_i_Val_minus']
-kstar_plus_val = stats['kstar_Val_plus']
-kstar_minus_val = stats['kstar_Val_minus']
-
-critical_quantile = 1-1e-5
-Upsilon_star_plus = np.quantile(Upsilon_i_plus, critical_quantile)
-Upsilon_star_minus = np.quantile(Upsilon_i_minus, critical_quantile)
-
-#%% compute overdensities & IV
-
-VALIDATION                                     = reference_data.shape[0]        # Number of samples to use for validation 
-K_M                                            = 1000            # Maximum value of kstar to consider
-KSTAR_RANGE                                    = range(20, K_M) # Range of kstar values to consider
-NUMBER_CORES                                   = 12  
-PARTITION_SIZE                                 = 100
-
-binary_sequences                               = From_data_to_binary.create_binary_array_cdist(data_with_anomaly, reference_data, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
-stats                                          = EagleEye_v7.calculate_p_values(binary_sequences, kstar_range=KSTAR_RANGE, validation=VALIDATION)
-
-#%% equalize the overdensities
-unique_elements_overdensities = EagleEye_v7.iterative_equalization(
-    data_with_anomaly, 
-    reference_data, 
-    stats['Upsilon_i_plus'], 
-    Upsilon_star_plus,
-    K_M,
-    NUMBER_CORES,
-    PARTITION_SIZE
-)
+# #%% get the thresholds
+# VALIDATION                                     = None        # Number of samples to use for validation 
+# K_M                                            = 1000            # Maximum value of kstar to consider
+# KSTAR_RANGE                                    = range(20, K_M) # Range of kstar values to consider
+# NUMBER_CORES                                   = 12  
+# PARTITION_SIZE                                 = 100
+# #binary_sequences                               = From_data_to_binary.create_binary_array_cdist(test_samples, reference_samples, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
+# num_sequences = 200000
+# binary_sequences                               = np.random.binomial(n=1, p=0.5, size=(num_sequences, K_M))
+# stats                                          = EagleEye_v7.calculate_p_values(binary_sequences, kstar_range=KSTAR_RANGE, validation=VALIDATION)
 
 
-#%% compute underdensities & IV
+# Upsilon_i_plus = stats['Upsilon_i_plus']
+# Upsilon_i_minus = stats['Upsilon_i_minus']
+# kstar_plus = stats['kstar_plus']
+# kstar_minus = stats['kstar_minus']
 
-binary_sequences_reverse                       = From_data_to_binary.create_binary_array_cdist(reference_data, data_with_anomaly, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
-stats_reverse                                  = EagleEye_v7.calculate_p_values(binary_sequences_reverse, kstar_range=KSTAR_RANGE, validation=VALIDATION)
+# # Unpack validation statistics
+# Upsilon_i_plus_val = stats['Upsilon_i_Val_plus']
+# Upsilon_i_minus_val = stats['Upsilon_i_Val_minus']
+# kstar_plus_val = stats['kstar_Val_plus']
+# kstar_minus_val = stats['kstar_Val_minus']
 
-#%% equalize the underdensities
-unique_elements_underdensities = EagleEye_v7.iterative_equalization(
-    reference_data, 
-    data_with_anomaly, 
-    stats_reverse['Upsilon_i_plus'], 
-    Upsilon_star_plus,
-    K_M,
-    NUMBER_CORES,
-    PARTITION_SIZE
-)
+# critical_quantile = 1-1e-5
+# Upsilon_star_plus = np.quantile(Upsilon_i_plus, critical_quantile)
+# Upsilon_star_minus = np.quantile(Upsilon_i_minus, critical_quantile)
 
-#%% get back
-smoothing = 3
-REGION_UNDER, REGION_OVER = EagleEye_v7.compute_anomalous_region(reference_data,
-                                                                 data_with_anomaly,
-                                                                 stats['Upsilon_i_minus'],
-                                                                 Upsilon_star_minus,
-                                                                 stats['Upsilon_i_plus'],
-                                                                 Upsilon_star_plus,
-                                                                 unique_elements_overdensities,
-                                                                 unique_elements_underdensities,
-                                                                 smoothing,
-                                                                 NUMBER_CORES,
-                                                                 PARTITION_SIZE)
+# #%% compute overdensities & IV
+
+# VALIDATION                                     = reference_data.shape[0]        # Number of samples to use for validation 
+# K_M                                            = 1000            # Maximum value of kstar to consider
+# KSTAR_RANGE                                    = range(20, K_M) # Range of kstar values to consider
+# NUMBER_CORES                                   = 12  
+# PARTITION_SIZE                                 = 100
+
+# binary_sequences                               = From_data_to_binary.create_binary_array_cdist(data_with_anomaly, reference_data, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
+# stats                                          = EagleEye_v7.calculate_p_values(binary_sequences, kstar_range=KSTAR_RANGE, validation=VALIDATION)
+
+# #%% equalize the overdensities
+# unique_elements_overdensities = EagleEye_v7.iterative_equalization(
+#     data_with_anomaly, 
+#     reference_data, 
+#     stats['Upsilon_i_plus'], 
+#     Upsilon_star_plus,
+#     K_M,
+#     NUMBER_CORES,
+#     PARTITION_SIZE
+# )
+
+
+# #%% compute underdensities & IV
+
+# binary_sequences_reverse                       = From_data_to_binary.create_binary_array_cdist(reference_data, data_with_anomaly, num_neighbors=K_M, num_cores=NUMBER_CORES, validation=VALIDATION,partition_size=PARTITION_SIZE)
+# stats_reverse                                  = EagleEye_v7.calculate_p_values(binary_sequences_reverse, kstar_range=KSTAR_RANGE, validation=None)
+
+# #%% equalize the underdensities
+# unique_elements_underdensities = EagleEye_v7.iterative_equalization(
+#     reference_data, 
+#     data_with_anomaly, 
+#     stats_reverse['Upsilon_i_plus'], 
+#     Upsilon_star_plus,
+#     K_M,
+#     NUMBER_CORES,
+#     PARTITION_SIZE
+# )
+
+# #%% get back
+# smoothing = 3
+# REGION_UNDER, REGION_OVER = EagleEye_v7.compute_anomalous_region(reference_data,
+#                                                                  data_with_anomaly,
+#                                                                  stats['Upsilon_i_minus'],
+#                                                                  Upsilon_star_minus,
+#                                                                  stats['Upsilon_i_plus'],
+#                                                                  Upsilon_star_plus,
+#                                                                  unique_elements_overdensities,
+#                                                                  unique_elements_underdensities,
+#                                                                  smoothing,
+#                                                                  NUMBER_CORES,
+#                                                                  PARTITION_SIZE)
             
-#%% plotting
+#%% plotting results
 
 #%% 3D scatter plot of the data and injected overdensities
+
+# Display the results
+critical_quantile         = res['critical_quantile']  
+stats                     = res['stats'] # Dict containing all upsilon+ and upsilon- values
+stats_reverse             = res['stats_reverse']
+Upsilon_star_plus         = res['Upsilon_star_minus']
+Upsilon_star_minus        = res['Upsilon_star_plus']
+Upsilon_i_plus_null       = res['Upsilon_i_minus_null']
+Upsilon_i_minus_null      = res['Upsilon_i_plus_null']
+REGION_UNDER              = res['REGION_UNDER']
+REGION_OVER               = res['REGION_OVER']           
+unique_elements_overdensities = res['unique_elements_overdensities']
+unique_elements_underdensities= res['unique_elements_underdensities']
+
 
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
@@ -296,8 +316,3 @@ cbar = plt.colorbar(scatter, ax=ax, pad=0.1)
 cbar.set_label('Upsilon_i_plus Value')  # Optional: Add a label to the colorbar
 
 plt.show()
-
-
-
-
-
